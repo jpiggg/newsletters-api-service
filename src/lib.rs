9f341010e719231edbd2bf96d7d05f57b962063d
jpig <isdolinskayaym@gmail.com>
2025-05-08 11:15:17 +0200

    registered actix routes as services

--- src/lib.rs
+++ src/lib.rs
@@ -2,21 +2,39 @@ use actix_web::{web, App, HttpRequest, HttpResponse, HttpServer, Responder};
 use actix_web::dev::Server;
 use std::net::TcpListener;
 
+
+#[derive(serde::Deserialize)]
+struct FormData {
+    name: String,
+    email: String
+}
+
+
+#[actix_web::get("/health_check")]
+async fn health_check() -> HttpResponse {
+    HttpResponse::Ok().finish()
+}
+
+#[actix_web::post("/subscriptions")]
+async fn subscribe(_form: web::Form<FormData>) -> HttpResponse {
+    HttpResponse::Ok().finish()
+}
+
 async fn greet(req: HttpRequest) -> impl Responder {
     let name = req.match_info().get("name").unwrap_or("World");
     format!("Hello {}!", &name)
 }
 
-async fn health_check() -> HttpResponse {
-    HttpResponse::Ok().finish()
-}
-
 pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
     let server = HttpServer::new(|| {
         App::new()
-            .route("/", web::get().to(greet))
-            .route("/health_check", web::get().to(health_check))
-            .route("/{name}", web::get().to(greet))
+            .service(health_check)
+            .service(subscribe)
+            .service(
+                actix_web::web::resource(vec!["/", "/{name}"])
+                .route(actix_web::web::get().to(greet))
+                .route(actix_web::web::get().to(greet))
+            )
     })
     .listen(listener)?
     .run();
